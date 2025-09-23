c21a4d062aa9b60e5d41e6799617e6aaa915a76c
ne0chem <brenkoangelina@gmail.com>
2025-09-24 00:06:14 +0300

    отредактировала страницу услуги

--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Setvice.css";
 import ServicesPage from "../../component/ServiceComponent/ServicesPage.jsx";
+import Zayavka from "../../component/Zayavka/Zayavka.jsx";
 
 const Service = () => {
   return (
     <div className="home-page">
       <ServicesPage />
+      <Zayavka />
     </div>
   );
 };
